1af9475c489edb3c0856928e392b349ecf3aff22
Doniyor <ddonierov95@gmail.com>
2025-12-17 13:58:38 +0500

    start of project

---
+++ backend/user/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'Username @{self.username}'
